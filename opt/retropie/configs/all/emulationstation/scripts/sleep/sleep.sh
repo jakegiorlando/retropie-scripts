f6f5109a2e9eec4021e6f0e94626e817ad125217
Jake Giorlando <54298177+jakegiorlando@users.noreply.github.com>
2025-07-07 15:43:41 -0400

    Created opt/retropie/configs/all/emulationstation/scripts/sleep/sleep.sh opt/retropie/configs/all/emulationstation/scripts/wake/wake.sh

---
+++ opt/retropie/configs/all/emulationstation/scripts/sleep/sleep.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#vcgencmd display_power 0 > /dev/null
+tvservice -o > /dev/null
+# Only works if "Screensaver After" value is non-zero
---
+++ opt/retropie/configs/all/emulationstation/scripts/wake/wake.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#vcgencmd display_power 1 > /dev/null
+tvservice -p
+# Only works if "Screensaver After" value is non-zero
